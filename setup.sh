f5a6594c809f477763135c38c79a37d43d260c4a
lululoid <ereiwn@gmail.com>
2025-01-08 13:54:39 +0700

    Update: Adjust for deprecation issue

--- setup.sh
+++ setup.sh
@@ -166,10 +166,16 @@ setup_softwares() {
 	# Installing additional software
 	logger "Installing additional software..." "INFO"
 	apt install -y neovim
+
 	logger "Installing chrome..." "INFO"
-	wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add - &&
-		sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list' &&
-		sudo apt update && sudo apt install -y google-chrome-stable
+	# Download the Google Linux package signing key and place it in the keyrings directory
+	wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo tee /usr/share/keyrings/google-linux-keyring.gpg >/dev/null
+
+	# Add the Google Chrome repository to the sources list with a keyring reference
+	echo "deb [signed-by=/usr/share/keyrings/google-linux-keyring.gpg arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list >/dev/null
+
+	# Update and install Google Chrome
+	sudo apt update && sudo apt install -y google-chrome-stable
 }
 
 add_zram() {
